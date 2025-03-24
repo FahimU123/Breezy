bf2e2de2ee6ee483f1103a8337ff6c34092bb27d
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-03-24 19:18:14 -0400

    stable

---
+++ Breezy/App/BreezyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezyApp.swift
+//  Breezy
+//
+//  Created by Fahim Uddin on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BreezyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breezy/Weather/WeatherDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherDataModel.swift
+//  Breezy
+//
+//  Created by Fahim Uddin on 3/24/25.
+//
+
+import Foundation
